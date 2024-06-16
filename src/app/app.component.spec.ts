1ad47e032dace2c9d462362fbb7f2fe43a4161ef
Anil <anilkumariz4u@gmail.com>
2024-06-16 19:42:09 -0400

    Created build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Bus-Booking-App-Angular'`, () => {
+  it(`should have as title 'Bus-Booking-App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Bus-Booking-App-Angular');
+    expect(app.title).toEqual('Bus-Booking-App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Bus-Booking-App-Angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Bus-Booking-App app is running!');
   });
 });
